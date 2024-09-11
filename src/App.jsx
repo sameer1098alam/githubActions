63179914e9f5a7ab605f071d99c229f918800c50
Sameer Alam <104647142+sameer1098alam@users.noreply.github.com>
2024-09-11 14:40:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p> hello react welcome to my application</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
